f04b146db6f208953e2107b642b9973680d68845
cnlohr <lohr85@gmail.com>
2018-08-05 14:52:10 -0400

    bump gen2 process

---
+++ gen2_process/fx3process.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdint.h>
+
+unsigned long histogram[100];
+int currun = 0;
+
+int main()
+{
+	FILE * f = fopen( "Data_20180805134454.8.dat", "rb" );
+	uint8_t lv = fgetc( f );
+	currun = 1;
+	while( !feof( f ) )
+	{
+		uint8_t v = fgetc( f );
+		if( lv != v )
+		{
+			if( currun >= 100 ) currun = 99;
+			histogram[currun]++;
+			currun = 1;
+		}
+		else
+			currun++;
+		lv = v;
+	}
+
+	int i;
+	for( i = 0; i < 100; i++ )
+	{
+		printf( "%d\t%lu\n", i, histogram[i] );
+	}
+}
+
